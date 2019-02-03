6b74e188161cd5eabf59cec1c893b13b321b6374
Lucaci Diana <diana.lucaci22@yahoo.com>
2019-02-02 21:13:09 -0500

    dif between nltk tok and twee_tok

--- twitter_tokenizer.py
+++ twitter_tokenizer.py
@@ -1,34 +1,38 @@
 from nltk.tokenize import TweetTokenizer
+from nltk import word_tokenize
+
 import io
 from collections import Counter
 import json
 #######################
 # a. Tokenizer        #
 #######################
-def get_corpus(file="twittercorpus.txt"):
+def get_corpus(file="data/twittercorpus.txt"):
 	with io.open(file, "r", encoding="utf8") as f:
 		corpus = f.read()
 	return corpus
 
-def nltk_tok(result_file="microblog2011_tokenized.txt"):
+def nltk_tok(result_file="data/microblog2011_tokenized.txt"):
 	tokenizer = TweetTokenizer()
 	corpus = get_corpus()
 	with io.open(result_file, "w",
 							 encoding="utf8") as f:
-		# f.write("#################################\n")
-		# f.write("# First 20 sentences tokenized: #\n")
-		# f.write("#################################\n")
-		f.write("#################################\n")
-		f.write("# Tokenizer result:             #\n")
-		f.write("#################################\n")		
-		for msg in corpus.split("\n"):
-			f.write('%s\n' %str.join(' ', tokenizer.tokenize(msg)))
+		with io.open("data/dif_tweettok_vs_tok.txt", "w", encoding="utf8") as dif:	
+			for msg in corpus.split("\n"):
+				tweet_tok = tokenizer.tokenize(msg)
+				tok = word_tokenize(msg)
+				f.write('%s\n' % str.join(' ', tweet_tok))
+				if len(tweet_tok) != len(tok):
+					dif.write("tweet_tok:\t%s\nnltk_tok:\t%s\n" %
+										(str.join(' ', tweet_tok), str.join(' ', tok)))
+					dif.write("#################################\n")
 
 		tokens = tokenizer.tokenize(corpus)
 		#f.write(str(Counter(tokens)))
 	return tokens
 
 tokens = nltk_tok()
+
 
 def regular_tok():
 	from nltk import word_tokenize
@@ -43,9 +47,11 @@ def stats(tokens):
 	""" The type/token ratio is defined as the number of types
 	 divided by the number of tokens."""
 	type_token_ration = unique_tokens / total_tokens
-	with open("stats_b.txt", "w") as f:
+	with open("data/stats_b.txt", "w") as f:
 		f.write("""Total tokens: %s\nUnique tokens: %s\nType/token ration: %s
 						""" % (str(total_tokens), str(unique_tokens),
 									 str(type_token_ration)))
 
 stats(tokens)
+
+###
