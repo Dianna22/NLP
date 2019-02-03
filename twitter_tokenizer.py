d2c5f116467a0a4601a6fe09926a1796ad2cf4aa
Lucaci Diana <diana.lucaci22@yahoo.com>
2019-02-02 20:49:15 -0500

    tokenizer+stats

--- twitter_tokenizer.py
+++ twitter_tokenizer.py
@@ -2,7 +2,9 @@ from nltk.tokenize import TweetTokenizer
 import io
 from collections import Counter
 import json
-
+#######################
+# a. Tokenizer        #
+#######################
 def get_corpus(file="twittercorpus.txt"):
 	with io.open(file, "r", encoding="utf8") as f:
 		corpus = f.read()
@@ -13,16 +15,37 @@ def nltk_tok(result_file="microblog2011_tokenized.txt"):
 	corpus = get_corpus()
 	with io.open(result_file, "w",
 							 encoding="utf8") as f:
+		# f.write("#################################\n")
+		# f.write("# First 20 sentences tokenized: #\n")
+		# f.write("#################################\n")
 		f.write("#################################\n")
-		f.write("# First 20 sentences tokenized: #\n")
-		f.write("#################################\n")
-		for msg in corpus.split("\n")[:20]:
-			f.write("%s\n" % str(tokenizer.tokenize(msg)))
-		f.write("#################################\n")
-		f.write("# Tokenizer report:             #\n")
-		f.write("#################################\n")
-		tokens = tokenizer.tokenize(corpus)
-		f.write(str(Counter(tokens)))
-		return tokens
+		f.write("# Tokenizer result:             #\n")
+		f.write("#################################\n")		
+		for msg in corpus.split("\n"):
+			f.write('%s\n' %str.join(' ', tokenizer.tokenize(msg)))
 
-nltk_tok()
+		tokens = tokenizer.tokenize(corpus)
+		#f.write(str(Counter(tokens)))
+	return tokens
+
+tokens = nltk_tok()
+
+def regular_tok():
+	from nltk import word_tokenize
+
+#######################
+# b. Stats            #
+#######################
+def stats(tokens):
+	count = Counter(tokens)
+	total_tokens = sum(count.values())
+	unique_tokens = len(set(tokens))
+	""" The type/token ratio is defined as the number of types
+	 divided by the number of tokens."""
+	type_token_ration = unique_tokens / total_tokens
+	with open("stats_b.txt", "w") as f:
+		f.write("""Total tokens: %s\nUnique tokens: %s\nType/token ration: %s
+						""" % (str(total_tokens), str(unique_tokens),
+									 str(type_token_ration)))
+
+stats(tokens)
